8fb2203955af2c16b6d889db7eb6d6c315b127f5
Riaan Deventer <124301932+riaandeventer@users.noreply.github.com>
2023-02-10 16:40:43 +0200

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('personal.urls')),
-    path('blog/', include('blog.urls')),
-    path('user_auth/', include("django.contrib.auth.urls")),
-    path('user_auth/', include("user_auth.urls", namespace='user_auth')),
+    path('blog', include('blog.urls')),
+    path('user_auth', include("django.contrib.auth.urls")),
+    path('user_auth', include("user_auth.urls", namespace='user_auth')),
 ]
